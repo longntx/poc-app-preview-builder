670a0f56e8dac45914290b16fd570ad824610f20
Long Nguyễn Thế Xuân <long@dreamhack.vn>
2022-02-23 19:25:47 +0700

    Try to add bookmark icon for ios

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
 import '../scss/globals.scss';
 import App from 'next/app';
+import React from 'react';
 import { resetServerContext } from 'react-beautiful-dnd';
+import Head from 'next/head';
+import AppleTouchIcon from 'public/apple-touch-icon.png';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          href={AppleTouchIcon.src}
+        />
+        <title>POC</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 MyApp.getInitialProps = async (appContext) => {
